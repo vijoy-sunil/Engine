c25b81994dc9ba3f53c0b8476be88d2da3f69391
vijoy-sunil <vijoy.sunil@gmail.com>
2024-09-04 12:43:28 +0530

    checklist fix

--- SandBox/Configs/ENEnvironment.h
+++ SandBox/Configs/ENEnvironment.h
@@ -1,14 +1,14 @@
 #ifndef EN_ENVIRONMENT_H
 #define EN_ENVIRONMENT_H
 
-namespace SandBox {
-    struct CameraSettings {
-
-    } g_cameraSettings;
-    
+namespace SandBox {    
     struct GridSettings {
         const char* vertexShaderBinaryPath   = "Build/Bin/gridShaderVert.spv";
         const char* fragmentShaderBinaryPath = "Build/Bin/gridShaderFrag.spv";
     } g_gridSettings;
+
+    struct CameraSettings {
+
+    } g_cameraSettings;
 }   // namespace SandBox
 #endif  // EN_ENVIRONMENT_H
